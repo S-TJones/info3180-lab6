b34de545275a6b8f73e44b31e6a873980a7fde6d
Shemar Jones <jones.shemar.sj@gmail.com>
2021-04-12 21:56:48 -0500

    Added HTML and other stuff

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,9 +1,114 @@
 /* Add your Application JavaScript */
+const Home = {
+  name: 'Home',
+  template: `
+    <div class="home">
+
+      <img src="/static/images/logo.png" alt="VueJS Logo">
+      <h1>
+        {{ welcome }}
+      </h1>
+
+    </div>
+  `,
+  data() {
+    return {
+      welcome: 'Hello World! Welcome to VueJS'
+    }
+  }
+};
+
+const NewsList = {
+  name: 'Newslist',
+  template: `
+    <h2 class= "n">
+      My News
+    </h2>
+
+    <div class="form-inline d-flex justify-content-center">
+
+      <div class="form-group mx-sm-3 mb-2">
+        
+        <label class="sr-only" for="search">Search</label>
+        <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+        
+        <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+      </div>
+
+    </div>
+
+    <div class="newnews">
+
+      <div v-for="article in articles" class="news__item">
+        <h5 class="card-title">
+          {{ article.title }}
+        </h5>
+
+        <img class="card-img-top" :src= article.urlToImage alt="Card image cap">
+        
+        <p class="card-text">
+          {{ article.description }}
+        </p>
+      </div>
+      
+    </div>
+  `,
+  created() {
+    let self = this;
+
+    fetch('https://newsapi.org/v2/top-headlines?country=us',{
+      headers: {
+      'Authorization': 'Bearer '
+      }
+    }).then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      console.log(data);
+
+      self.articles = data.articles;
+    });
+  },
+  data() {
+    return {
+      articles: [], searchTerm: ''
+    }
+  }, 
+  methods: {
+    searchNews() {
+      let self = this;
+      fetch('https://newsapi.org/v2/everything?q=' + self.searchTerm + '&language=en', {
+        headers: {
+          'Authorization': 'Bearer '
+        }
+      }).then(function(response) {
+        return response.json();
+      }).then(function(data) {
+        console.log(data);
+        self.articles = data.articles;
+      });
+    }
+  }
+};
+
+
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/news', component: NewsList }
+  ]
+});
+  
 const app = Vue.createApp({
   data() {
     return {
       welcome: 'Hello World! Welcome to VueJS'
     }
+  }, 
+  components: {
+    'home': Home,
+    'news-list': NewsList
   }
 });
 
@@ -45,10 +150,11 @@ app.component('app-footer', {
       </footer>
   `,
   data: function() {
-      return {
-          year: (new Date).getFullYear()
-      }
+    return {
+      year: (new Date).getFullYear()
+    }
   }
 })
 
+app.use(router);
 app.mount('#app');
